4f63fe3b25daac08f88f021fc65b8febcbf4cb95
bautomass <33027482+bautomass@users.noreply.github.com>
2024-12-10 22:10:20 +0200

    modified:   src/app/sakt-projekta-skici/page.tsx

--- src/app/sakt-projekta-skici/page.tsx
+++ src/app/sakt-projekta-skici/page.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import Head from "next/head";
 import Header from "../../components/Header";
 import SketchBuilder from "../../components/SketchBuilder";
-import SEOMetadata from "../../components/SEOMetadata";
 
 export default function SketchToolPage() {
   return (
